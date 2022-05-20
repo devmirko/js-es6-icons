681880bd3b5f5c5645800863bd604b0ce1eede71
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-20 09:31:03 +0200

    fine esercizio

--- data.js
+++ data.js
@@ -115,58 +115,63 @@ const icone = [
 ];
 
 
-//Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
-
+//creo variabili elementi html
 const containerIcon = document.getElementById("icone_container");
 const containerSel =  document.getElementById("selettore_icone");
-const scelta =  containerSel.value;
 
 
-insertBox();
+//creo addEventListener sul selettore
+containerSel.addEventListener("change",filterArray);
+ // imposto il valore iniziale del selettore su all
+ containerSel.value = 'all';
+ // stampo tutti gli elementi sulla pagina
+ printElement(icone);
 
-containerSel.addEventListener("change",
-function () {
 
-	// containerIcon.innerHTML = "";
-
-	if (scelta == "all") {
-		insertBox();
-	} else {
-		insertFilterBox()
+//funzione che va a filtrare gli elementi a seconda del value e crea una array con gli elementi, che poi vengono stampati 
+function filterArray() {
+	switch (containerSel.value) {
+		    case 'animal':
+			    const arrAnimal = icone.filter((element) => element.type === 'animal');
+			    printElement(arrAnimal);
+			    break;
+			case 'vegetable':
+				const arrVegetable = icone.filter((element) => element.type === 'vegetable');
+				printElement(arrVegetable);
+				break;
+			case 'user':
+				const arrUser = icone.filter((element) => element.type === 'user');
+				printElement(arrUser);
+				break;
+		
+	
+		default:
+			printElement(icone);
 	}
+	
 }
-)
-//creo una funzione che genera le card
-function generatorBox (icons) {
-     return `
-	 <div class="box">
-		 <i class="${icons.family} ${icons.prefix}${icons.name}" style="color: ${icons.color};"></i>
-		 <div class="name">${icons.name}</div>
-	 </div>
-	 `;
-	 }
 
-	 //inserisci le box
 
-	 function insertBox() {
-		 icone.forEach((icon) => {
-			containerIcon.innerHTML += generatorBox(icon);
-		});
-		 
-	 }
+//funzione che crea gli elementi html e aggiunge le classi che creano le icone.
+function printElement(icone) {
+	
+	containerIcon.innerHTML = "";
 
-	 //filtra le box in base alla scelta
-	 function filtro(box) {
-		 icone.filter((box) => {
-			return box.type == scelta.value;
-		});
-	 }
+	icone.forEach(element => {
+		const card = document.createElement("div");
+		card.classList.add("card");
+		const icon = document.createElement("i");
+		const nome = document.createElement("div");
+		nome.classList.add("name");
+		nome.innerText = element.name.toUpperCase();
+		//unisci le classi per creare le icone 
+		icon.classList.add(element.family);
+		icon.classList.add(element.prefix + element.name);
+		icon.classList.add(element.color);
+		card.append(icon);
+		card.append(nome);
+		containerIcon.append(card);
 
-	 //inserisci le box filtrate
-	 function insertFilterBox() {
-		 let boxes = filtro();
-		 boxes.forEach((box) => {
-			containerIcon.innerHTML += generatorBox(box);
-		});
+	})
 
-	} 
+}
